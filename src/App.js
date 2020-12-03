f5a254adbd94c00750d52a8331069231972cf5cd
noahrmitchell <noahrmitchell@gmail.com>
2020-12-03 21:54:34 +0000

    Set up React Router Dom with the Nav and two blank Our Work and Contact Us pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import AboutUs from './pages/AboutUs';
+import OurWork from './pages/OurWork';
+import ContactUs from './pages/ContactUs';
 import GlobalStyle from "./components/GlobalStyle";
 import Nav from './components/Nav';
+//Routing 
+import { Switch, Route } from 'react-router-dom'; 
 
 function App() {
   return (
     <div className="App">
       <Nav />
-      <AboutUs />;
+        <Switch>
+        <Route path="/" exact>
+          <AboutUs />;
+        </Route>
+        <Route path="/work">
+          <OurWork />;
+        </Route>
+        <Route path="/contact">
+        <ContactUs />;
+        </Route>
+      </Switch>
       <GlobalStyle />;
     </div>
   );
