3be8bfe1073646cb8058e6493b7090451fd5a89c
noahrmitchell <noahrmitchell@gmail.com>
2020-12-06 07:50:10 +0000

    Added component and routing to project movie details pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import OurWork from './pages/OurWork';
 import ContactUs from './pages/ContactUs';
 import GlobalStyle from "./components/GlobalStyle";
 import Nav from './components/Nav';
+import MovieDetail from './pages/MovieDetail';
 //Routing 
 import { Switch, Route } from 'react-router-dom'; 
 
@@ -15,8 +16,11 @@ function App() {
         <Route path="/" exact>
           <AboutUs />;
         </Route>
-        <Route path="/work">
+        <Route path="/work" exact>
           <OurWork />;
+        </Route>
+        <Route>
+          <MovieDetail path="/work/:id" />
         </Route>
         <Route path="/contact">
         <ContactUs />;
